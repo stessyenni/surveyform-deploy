581e1445f1e6a65534b1f4dfbf0f2645da9ea209
Stessy Enni <50053328+stessyenni@users.noreply.github.com>
2025-12-12 14:00:06 +0100

    updates

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -6,7 +6,7 @@ function Header () {
     <div className="Header">
       <nav className="Menu">
         <div>
-          <ul className="flex flex-row gap-10 items-center justify-center">
+          <ul className="flex flex-row gap-10 items-center justify-center hover:bg-amber-400">
             <Link href="/"> Survey Form</Link>
 
             <Link href="/view"> Survey</Link>
