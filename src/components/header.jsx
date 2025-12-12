23dbfbcc7cc3f1ec8fa656637c9b2a5d5073d1e1
Stessy Enni <50053328+stessyenni@users.noreply.github.com>
2025-12-12 13:07:37 +0100

    header edit

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Header () {
   return (
-    <div className='Header'>
+    <div className='Header flex flex-row gap-10 items-center justify-center'>
       <nav className='Menu'>
         <div>
           <ul>
