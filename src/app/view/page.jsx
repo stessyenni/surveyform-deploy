6f2f5e74a7c5083e4cd95c377d2ff043a14ab917
Stessy Enni <50053328+stessyenni@users.noreply.github.com>
2025-12-12 14:07:47 +0100

    table head

--- src/app/view/page.jsx
+++ src/app/view/page.jsx
@@ -5,11 +5,17 @@ const Survey = () => {
     <div>
       <table>
         <thead>
-          
+          <td>Full Name</td>
+          <td>Age</td>
+          <td>Email</td>
+          <td>Education</td>
+          <td>Department</td>
+          <td>Jobtitle</td>
+          <td>Jobtype</td>
         </thead>
       </table>
     </div>
-  )
+  );
 }
 
 export default Survey
