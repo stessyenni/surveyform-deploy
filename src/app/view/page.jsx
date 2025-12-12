8778b22dc168a6b2735404dc816ce826f6fd6f38
Stessy Enni <50053328+stessyenni@users.noreply.github.com>
2025-12-12 14:16:37 +0100

    DUMMY DATA

--- src/app/view/page.jsx
+++ src/app/view/page.jsx
@@ -1,21 +1,65 @@
 import React from 'react'
 
 const Survey = () => {
-  return (
-    <div>
-      <table>
-        <thead>
-          <td>Full Name</td>
-          <td>Age</td>
-          <td>Email</td>
-          <td>Education</td>
-          <td>Department</td>
-          <td>Jobtitle</td>
-          <td>Jobtype</td>
-        </thead>
-      </table>
-    </div>
-  );
+
+const dummyData = [
+  {
+    fullname: "John Doe",
+    age: 28,
+    email: "johndoe@example.com",
+    education: "Bachelor's Degree",
+    department: "Marketing",
+    jobtitle: "Marketing Analyst",
+    jobtype: "Full-Time",
+  },
+  {
+    fullname: "Sarah Kim",
+    age: 34,
+    email: "sarah.kim@example.com",
+    education: "Master's Degree",
+    department: "Human Resources",
+    jobtitle: "HR Manager",
+    jobtype: "Part-Time",
+  },
+  {
+    fullname: "Michael Smith",
+    age: 41,
+    email: "michael.smith@example.com",
+    education: "PhD",
+    department: "Research & Development",
+    jobtitle: "Lead Researcher",
+    jobtype: "Contract",
+  },
+];
+
+return (
+  <div>
+    <table>
+      <thead>
+        <td>Full Name</td>
+        <td>Age</td>
+        <td>Email</td>
+        <td>Education</td>
+        <td>Department</td>
+        <td>Jobtitle</td>
+        <td>Jobtype</td>
+      </thead>
+      <tbody>
+        {dummyData.map((data, index) => (
+          <tr key={index}>
+            <td>{data.fullname}</td>
+            <td>{data.age}</td>
+            <td>{data.email}</td>
+            <td>{data.education}</td>
+            <td>{data.department}</td>
+            <td>{data.jobtitle}</td>
+            <td>{data.jobtype}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  </div>
+);
 }
 
 export default Survey
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -9,7 +9,7 @@ function Header () {
           <ul className="flex flex-row gap-10 items-center justify-center hover:bg-amber-400">
             <Link href="/"> Survey Form</Link>
 
-            <Link href="/view"> Survey</Link>
+            <Link href="/view"> Survey List</Link>
           </ul>
         </div>
       </nav>
